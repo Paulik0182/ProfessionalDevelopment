4470fb141eec84c9e84adcdb8719fa5e3cbced3c
Paul <paulik0182@gmail.com>
2023-04-12 16:10:33 +0300

    Project setup. Connecting libraries RXJava, Retrofit, RecyclerView, ViewModel.

--- app/src/main/java/com/paulik/professionaldevelopment/ui/RootActivity.kt
+++ app/src/main/java/com/paulik/professionaldevelopment/ui/RootActivity.kt
@@ -1,9 +1,10 @@
-package com.paulik.professionaldevelopment
+package com.paulik.professionaldevelopment.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.paulik.professionaldevelopment.R
 
-class MainActivity : AppCompatActivity() {
+class RootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
