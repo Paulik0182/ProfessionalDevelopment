8270a201aa8709765a18af985959df42f7ea9819
Paul <paulik0182@gmail.com>
2023-04-25 22:59:02 +0300

    Correction cod

--- app/src/main/java/com/paulik/professionaldevelopment/data/rx/SchedulerProvider.kt
+++ app/src/main/java/com/paulik/professionaldevelopment/data/rx/SchedulerProvider.kt
@@ -1,6 +1,5 @@
 package com.paulik.professionaldevelopment.data.rx
 
-import com.paulik.professionaldevelopment.domain.rx.ISchedulerProvider
 import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
