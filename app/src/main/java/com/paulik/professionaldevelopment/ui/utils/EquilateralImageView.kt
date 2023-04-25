5ea5ac17836a92f3fb4f31f1929cbbec920dbeba
Paul <paulik0182@gmail.com>
2023-04-25 16:10:42 +0300

    Немного оптимизирован код кастомной View (EquilateralImageView)

--- app/src/main/java/com/paulik/professionaldevelopment/ui/utils/EquilateralImageView.kt
+++ app/src/main/java/com/paulik/professionaldevelopment/ui/utils/EquilateralImageView.kt
@@ -4,20 +4,24 @@ import android.content.Context
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
 
+/**
+ * Это кастомная View
+ *
+ * @JvmOverloads - аннотация позволяет автоматически генерировать несколько перегруженных методов
+ * в байткоде с различным числом аргументов. Аннотация @JvmOverloads доносит до компилятора Kotlin,
+ * что нужно сгенерировать перегруженные методы для всех возможных комбинаций аргументов,
+ * включая те, которые имеют значения по умолчанию.
+ */
+
 // TODO Требует пояснения
 
-class EquilateralImageView : AppCompatImageView {
+class EquilateralImageView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatImageView(context, attrs, defStyleAttr) {
 
-    constructor(context: Context) : super(context)
-
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
-
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr
-    )
-
+    /** Метод для отрисовки элемента, то-есть элемент делаем просто квадратным*/
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, widthMeasureSpec)
     }
