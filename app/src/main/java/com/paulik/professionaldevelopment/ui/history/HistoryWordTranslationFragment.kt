7311d2fb350728d208eb026e14da1b6f142e2603
Paul <paulik0182@gmail.com>
2023-04-26 10:48:29 +0300

    В HistoryWordTranslationFragment добавлен onCreateView.

--- app/src/main/java/com/paulik/professionaldevelopment/ui/history/HistoryWordTranslationFragment.kt
+++ app/src/main/java/com/paulik/professionaldevelopment/ui/history/HistoryWordTranslationFragment.kt
@@ -1,9 +1,12 @@
 package com.paulik.professionaldevelopment.ui.history
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import com.paulik.professionaldevelopment.AppState
+import com.paulik.professionaldevelopment.R
 import com.paulik.professionaldevelopment.data.HistoryWordTranslationInteractorImpl
 import com.paulik.professionaldevelopment.databinding.FragmentHistoryWordTranslationBinding
 import com.paulik.professionaldevelopment.domain.entity.DataEntity
@@ -18,6 +21,14 @@ class HistoryWordTranslationFragment :
 
     override val viewModel: HistoryWordTranslationViewModel by viewModel()
     private val adapter: HistoryWordTranslationAdapter by lazy { HistoryWordTranslationAdapter() }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_history_word_translation, container, false)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
